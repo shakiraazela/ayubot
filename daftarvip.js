16faed83942fff221a6c6cd0b6c8002a3a5e6215
shakiraazela <81231950+shakiraazela@users.noreply.github.com>
2021-03-28 08:59:19 +0700

    Add files via upload

---
+++ daftarvip.js
@@ -0,0 +1,30 @@
+const animesaran = () => { 
+	return `       
+	
+	┌──「 Daftarvip 」
+│ 
+├ ❏ Sewa : 10k/grup (bulan)
+├ ❏ Buat : 50k (bisa jadi owner)
+├ ❏ Pembayaran bisa melalui
+├ ❏ gopay, pulsa +5k (karena kena rate)
+│
+├ ❏──「 ᴋᴇᴜɴᴛᴜɴɢᴀɴ ᴅᴏɴᴀsɪ 」
+│
+├ ❏ 1. Bisa Menjadi user premium!
+├ ❏ 2. Bisa Menjadi User Donator
+│
+├ ❏──「 ᴋᴇᴜɴᴛᴜɴɢᴀɴ ᴅᴏɴᴀsɪ 」
+│
+├ ❏ 1. FITUR DONATOR KE BUKA
+├ ❏ 2. BISA MEMBAWA BOT KE GRUP
+├ ❏ 3. DAN MASIH BANYAK LAGI 
+│
+└──「 *AHMAD PRASETYO* 」
+
+*Jika Minat Hubungi Nomor Bang #owner*
+*https://api.whatsapp.com/send?phone=6287869984953*
+	
+	
+	`
+}
+exports.animesaran = animesaran
