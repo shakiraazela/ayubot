31d2e54b13cfe19796740ecabc020de34102c15e
shakiraazela <81231950+shakiraazela@users.noreply.github.com>
2021-03-28 09:02:03 +0700

    Add files via upload

---
+++ src/iklan.js
@@ -0,0 +1,31 @@
+const iklan = () => { 
+	return `           
+
+┌──「 𝙄𝙆𝙇𝘼𝙉 」
+│ 
+├ ❏ Sewa : 10k/grup (bulan)
+├ ❏ Buat : 50k (bisa jadi owner)
+├ ❏ Pembayaran bisa melalui
+├ ❏ gopay, pulsa +5k (karena kena rate)
+│
+├ ❏──「 𝙆𝙀𝙐𝙉𝙏𝙐𝙉𝙂𝘼𝙉 𝙎𝙀𝙒𝘼 𝘽𝙊𝙏 」
+│
+├ ❏ 1. Bisa Menjadi user premium!
+├ ❏ 2. Bisa Mendapatkan command premium
+│
+├ ❏──「 𝙆𝙀𝙐𝙉𝙏𝙐𝙉𝙂𝘼𝙉 𝘽𝙐𝘼𝙏 𝘽𝙊𝙏 」
+│
+├ ❏ 1. Bisa menjadi owner bot seniri
+├ ❏ 2. Bisa mengganti nama bot sendiri
+├ ❏ 3. Bisa membawa bot ke grup sendiri
+├ ❏ 4. Bisa menggunakan command owner
+├ ❏ 5. Bisa menyewakan bot kembali
+│
+└──「 *AHMAD PRASETYO* 」
+
+*Jika Minat Hubungi Nomor Bang #owner*
+*https://api.whatsapp.com/send?phone=6287869984953*
+
+`
+}
+exports.iklan = iklan
